e4d99981b72b978d14a37475ed37e42b05990a2b
Dmytro Azarenkov <azarenkov.ds@gmail.com>
2025-07-24 14:28:49 -0400

    Added feed link to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,7 @@ function NavBar() {
       <NavLink to="/">Home</NavLink>
       <NavLink to="/register">Register</NavLink>
       <NavLink to="/login">Login</NavLink>
+      <NavLink to="/feed">Feeds</NavLink>
     </nav>
   );
 }
